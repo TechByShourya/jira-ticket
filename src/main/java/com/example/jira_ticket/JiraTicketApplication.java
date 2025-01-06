dd831f2871099952f5f105a6fa3d3b9cce2d722c
Shourya Goyal <shourya.goyal@decimal.co.in>
2025-01-06 22:59:37 +0530

    adding empty file

---
+++ src/main/java/com/example/jira_ticket/JiraTicketApplication.java
@@ -0,0 +1,11 @@
+package com.example.jira_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTicketApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JiraTicketApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/jira_ticket/JiraTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jira_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

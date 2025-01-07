a0ccf4a49cb4abb4c456570b256cb75bd8fe6e56
Shourya Goyal <shourya.goyal@decimal.co.in>
2025-01-07 15:39:02 +0530

    to create conflict

--- src/main/java/com/example/jira_ticket/JiraTicketApplication.java
+++ src/main/java/com/example/jira_ticket/JiraTicketApplication.java
@@ -8,6 +8,11 @@ public class JiraTicketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JiraTicketApplication.class, args);
 	}
+	public static void substtact(){
+		int a=5;
+		int b= 4;
+		System.out.println(a-b);
+	}
 }
 
 
